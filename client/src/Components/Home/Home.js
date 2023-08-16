5068627435a8d32d8c8d647724f6894f2b1c1686
ElBugs99 <jpvaldivia99@gmail.com>
2023-08-16 02:03:25 -0400

    installed cors and fixed hellish error with server

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   const [backendData, setBackendData] = useState([{}])
 
   useEffect(() => {
-    fetch('http://localhost:5000/api')
+    fetch('/api')
     .then(response => response.json())
     .then(data => setBackendData(data))
     .catch(error => console.log("error catching data", error))
