c9cfc3e1315938af07ea7c8be98f6da781558c7f
ElBugs99 <jpvaldivia99@gmail.com>
2023-09-26 18:18:27 -0300

    page not found route implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import NoMatch from "./Components/UI/NoMatch/NoMatch";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NoMatch />} />
         </Routes>
       </Router>
     </div>
