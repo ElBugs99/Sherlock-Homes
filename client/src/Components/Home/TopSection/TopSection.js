fb7c2e3ea8a103ed7878fb8a871fdd53b32d78d4
ElBugs99 <jpvaldivia99@gmail.com>
2023-09-30 17:50:52 -0300

    home top section css and image upgrade

--- client/src/Components/Home/TopSection/TopSection.js
+++ client/src/Components/Home/TopSection/TopSection.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './topSection.css'
+import img from '../../../assets/images/longApartmen.jpg';
 
 export default function TopSection() {
   return (
     <div className='topSection'>
-      TopSection
+      <div className='top-img-container'>
+        <img className='top-img' src={img} alt='promo' />
+      </div>
     </div>
   )
 }
