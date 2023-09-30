de902b3e731fcbbe81345d16a01eb7c7373d5820
ElBugs99 <jpvaldivia99@gmail.com>
2023-09-30 18:20:32 -0300

    navbar css advancement

--- client/src/Components/UI/NavBar/NavBar.js
+++ client/src/Components/UI/NavBar/NavBar.js
@@ -5,11 +5,14 @@ export default function NavBar() {
   return (
     <div className='navBar'>
       <div className='nav-options-container'>
-        <div>
-          <div className='logo'>LOGO</div>
+        <div className='nav-logo-container'>
+          <div className='nav-logo'>Sherlock Homes</div>
         </div>
         <div className='nav-options'>
-
+          <div className='nav-option'>option1</div>
+          <div className='nav-option'>option2</div>
+          <div className='nav-option'>option3</div>
+          <div className='nav-option'>option4</div>
         </div>
       </div>
     </div>
