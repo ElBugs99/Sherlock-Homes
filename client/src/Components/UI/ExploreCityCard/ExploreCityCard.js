d9eec25fa110c0c7ad333ca8dc104d195752d84c
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-02 19:17:09 -0300

    addded city card component

---
+++ client/src/Components/UI/ExploreCityCard/ExploreCityCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './exploreCityCard.css'
+
+export default function ExploreCityCard() {
+  return (
+    <div className='Explore-city-card'>
+      ExploreCityCard
+    </div>
+  )
+}
