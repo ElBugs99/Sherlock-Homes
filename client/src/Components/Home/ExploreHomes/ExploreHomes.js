a77f78e8eb7ce77178f45f28a042de23c30fc5bc
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-02 19:01:55 -0300

    explore section basic layout

--- client/src/Components/Home/ExploreHomes/ExploreHomes.js
+++ client/src/Components/Home/ExploreHomes/ExploreHomes.js
@@ -6,10 +6,10 @@ export default function ExploreHomes() {
     <div className='explore-homes'>
       <div className='cities-container'>
         <div className='cities-title'>
-            
+            TITLE
         </div>
         <div className='explore-cities'>
-
+            CITIES
         </div>
       </div>
     </div>
