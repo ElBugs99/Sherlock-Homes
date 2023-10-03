f1c0d2be282fe4dce673d8c067d7ff03f56bf767
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-03 18:35:11 -0300

    added serena city image asset

--- client/src/Components/Home/ExploreHomes/ExploreHomes.js
+++ client/src/Components/Home/ExploreHomes/ExploreHomes.js
@@ -3,6 +3,7 @@ import './exploreHomes.css'
 import ExploreCityCard from '../../UI/ExploreCityCard/ExploreCityCard'
 import vina from '../../../assets/images/vina.jpg'
 import santiago from '../../../assets/images/santiago.jpg'
+import serena from '../../../assets/images/la-serena.jpg'
 
 export default function ExploreHomes() {
   return (
@@ -14,7 +15,7 @@ export default function ExploreHomes() {
         <div className='explore-cities'>
             <ExploreCityCard cityName={'Viña del Mar'} listings={50} image={vina} />
             <ExploreCityCard cityName={'Santiago'} listings={50} image={santiago}/>
-            <ExploreCityCard cityName={'Viña del Mar'} listings={50} image={vina}/>
+            <ExploreCityCard cityName={'Viña del Mar'} listings={50} image={serena}/>
             <ExploreCityCard cityName={'Viña del Mar'} listings={50} image={vina}/>
         </div>
       </div>
