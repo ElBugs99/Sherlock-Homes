5bd14d2b704d19a3d478c4fdc0f2087e233a8c96
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-05 16:15:56 -0300

    video asset added at home page

--- client/src/Components/Home/TopSection/TopSection.js
+++ client/src/Components/Home/TopSection/TopSection.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './topSection.css'
 import img from '../../../assets/images/longApartmen.jpg';
+import homeVideo from '../../../assets/videos/home-video.mp4';
 
 export default function TopSection() {
   return (
     <div className='topSection'>
       <div className='top-img-container'>
-        <img className='top-img' src={img} alt='promo' />
+      <video className='top-img' autoPlay loop muted>
+        <source src={homeVideo} type="video/mp4" />
+        {/* Add additional source elements for other formats (WebM, Ogg) */}
+        Your browser does not support the video tag.
+      </video>
+        {/* <img className='top-img' src={img} alt='promo' /> */}
         <div className='top-img-text'>Test text</div>
       </div>
     </div>
