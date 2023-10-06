c0137950590d53f949493ea42593bad05d2a966c
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-05 21:39:35 -0300

    put overlay over home video

--- client/src/Components/Home/TopSection/TopSection.js
+++ client/src/Components/Home/TopSection/TopSection.js
@@ -6,13 +6,13 @@ import homeVideo from '../../../assets/videos/home-video.mp4';
 export default function TopSection() {
   return (
     <div className='topSection'>
+      <div className='top-overlay'>a</div>
       <div className='top-img-container'>
         <video className='top-img' autoPlay loop muted>
           <source src={homeVideo} type="video/mp4" />
           {/* Add additional source elements for other formats (WebM, Ogg) */}
           Your browser does not support the video tag.
         </video>
-        <div className='top-overlay'></div>
         {/* <img className='top-img' src={img} alt='promo' /> */}
         <div className='top-img-text'>Test text</div>
       </div>
