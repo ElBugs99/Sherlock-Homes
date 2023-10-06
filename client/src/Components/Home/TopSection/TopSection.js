c5263b82d17a66ada9d40f5b32ca230fe0f1cc81
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-05 21:08:38 -0300

    top section overlay added

--- client/src/Components/Home/TopSection/TopSection.js
+++ client/src/Components/Home/TopSection/TopSection.js
@@ -7,11 +7,12 @@ export default function TopSection() {
   return (
     <div className='topSection'>
       <div className='top-img-container'>
-      <video className='top-img' autoPlay loop muted>
-        <source src={homeVideo} type="video/mp4" />
-        {/* Add additional source elements for other formats (WebM, Ogg) */}
-        Your browser does not support the video tag.
-      </video>
+        <video className='top-img' autoPlay loop muted>
+          <source src={homeVideo} type="video/mp4" />
+          {/* Add additional source elements for other formats (WebM, Ogg) */}
+          Your browser does not support the video tag.
+        </video>
+        <div className='top-overlay'></div>
         {/* <img className='top-img' src={img} alt='promo' /> */}
         <div className='top-img-text'>Test text</div>
       </div>
