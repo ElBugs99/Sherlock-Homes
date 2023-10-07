557abfff9ff573ef4693bf02245fbd4967acadd6
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-07 19:54:28 -0300

    sidebar added to search page

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './search.css'
+import SideBar from '../UI/SideBar/SideBar'
 
 export default function Search() {
   return (
     <div className='search'>
-      search
+        <SideBar />
+        <div>search</div>
     </div>
   )
 }
