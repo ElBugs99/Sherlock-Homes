0003fff456764f1a2390575d9562b6bb7454202b
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-08 00:32:14 -0300

    added navbar in search page

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './search.css'
 import SideBar from '../UI/SideBar/SideBar'
+import NavBar from '../UI/NavBar/NavBar'
 
 export default function Search() {
   return (
     <div className='search'>
+        <NavBar />
         <div className='search-info'>
             <div className='search-info-title'>Results: 50</div>
             <SideBar />
