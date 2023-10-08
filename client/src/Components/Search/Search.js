cd658e6ca26f4769551a773d5fed526d9c0dd2f6
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-08 00:45:19 -0300

    search page css upgrade

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import './search.css'
-import SideBar from '../UI/SideBar/SideBar'
-import NavBar from '../UI/NavBar/NavBar'
+import React from "react";
+import "./search.css";
+import SideBar from "../UI/SideBar/SideBar";
+import NavBar from "../UI/NavBar/NavBar";
 
 export default function Search() {
   return (
-    <div className='search'>
-        <NavBar />
-        <div className='search-info'>
-            <div className='search-info-title'>Results: 50</div>
-            <SideBar />
+    <div className="search">
+      <NavBar />
+      <div className="search-container">
+        <div className="search-info">
+          <div className="search-info-title">Results: 50</div>
+          <SideBar />
         </div>
-        <div className='search-results'>search</div>
+        <div className="search-results">search</div>
+      </div>
     </div>
-  )
+  );
 }
