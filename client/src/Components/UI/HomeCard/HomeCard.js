457be131492f54debb4641b51237e152d33517c6
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-12 20:59:26 -0300

    home card component basic layout

--- client/src/Components/UI/HomeCard/HomeCard.js
+++ client/src/Components/UI/HomeCard/HomeCard.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import './homeCard.css'
+import React from "react";
+import "./homeCard.css";
+import img from "../../../assets/images/la-serena.jpg";
 
-export default function HomeCard({image, price,}) {
+export default function HomeCard({ image, price }) {
   return (
-    <div className='home-card'>
-      <div className='home-card-container'>
-        homeCard
+    <div className="home-card">
+      <div className="home-card-container">
+        <div className="home-card-img-container">
+          <img className="home-card-img" src={img} alt="home" />
+        </div>
+        <div className="home-card-info">info</div>
       </div>
     </div>
-  )
+  );
 }
