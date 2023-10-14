41f4d24d9aec23063830f7a881a3c12649ae04e8
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-13 21:49:44 -0300

    added image in 404 page

--- client/src/Components/UI/NoMatch/NoMatch.js
+++ client/src/Components/UI/NoMatch/NoMatch.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './noMatch.css'
+import img from '../../../assets/images/alien-obduction-svgrepo-com.svg'
 
 export default function NoMatch() {
   return (
     <div className='notFound'>
+      <div className='not-found-svg-container'>
+        <img className='not-found-svg' src={img} alt='ufo' />
+      </div>
      Error 404
      <div>página no encontrada</div>
     </div>
