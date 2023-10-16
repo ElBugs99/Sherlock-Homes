4f419f6d2fcdc738c59f0d0c43c1985e0e4f264d
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-16 17:19:54 -0300

    added functional letter carrousel in top section

--- client/src/Components/Home/TopSection/TopSection.js
+++ client/src/Components/Home/TopSection/TopSection.js
@@ -1,48 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import './topSection.css'
-import homeVideo from '../../../assets/videos/home-video.mp4';
-import search from '../../../assets/images/white-search-icon.svg';
+import React, { useEffect, useState } from "react";
+import "./topSection.css";
+import homeVideo from "../../../assets/videos/home-video.mp4";
+import search from "../../../assets/images/white-search-icon.svg";
 
 export default function TopSection() {
-
   const styles = {
     data: {
-      1: {title: 'Duplex', style: {color: 'blue'} },
-      2: {title: 'Casa', style: {color: 'red'}},
-      3: {title: 'Departamento', style: {color: 'brown'}},
+      1: { title: "Hogar", style: { color: "#38ba8c" } },
+      2: { title: "Duplex", style: { color: "#baa938" } },
+      3: { title: "Departamento", style: { color: "#ba383c" } },
+      4: { title: "Estudio", style: { color: "#38baba" } },
     },
-  }
+  };
 
-  const [title, setTitle] = useState(1)
+  const [title, setTitle] = useState(1);
 
   useEffect(() => {
-    
     const interval = setInterval(() => {
-      console.log('hola')
-    } ,2000)
+      try {
+        if (title > 3) {
+          setTitle(1);
+        } else {
+          setTitle((prev) => prev + 1);
+        }
+      } catch (error) {
+        // Handle the error
+        console.error(error);
+      }
+
+    }, 2000);
 
     return () => {
       clearInterval(interval);
-  }
-
-  })
+    };
+  }, [title]);
 
   return (
-    <div className='topSection'>
-      <div className='top-overlay'></div>
-      <div className='top-img-container'>
-        <video className='top-img' autoPlay loop muted>
+    <div className="topSection">
+      <div className="top-overlay"></div>
+      <div className="top-img-container">
+        <video className="top-img" autoPlay loop muted>
           <source src={homeVideo} type="video/mp4" />
           {/* Add additional source elements for other formats (WebM, Ogg) */}
           Your browser does not support the video tag.
         </video>
         {/* <img className='top-img' src={img} alt='promo' /> */}
-        <form className='top-form'>
-          <input className='top-input' spellCheck='false' />
-          <button className='search-btn'><img src={search} className='search-icon' alt='search' /></button>
+        <form className="top-form">
+          <input className="top-input" spellCheck="false" />
+          <button className="search-btn">
+            <img src={search} className="search-icon" alt="search" />
+          </button>
         </form>
-        <div className='top-img-text' style={styles.data[1].style}>Encuentra tu nuevo hogar {styles.data[1].title}.</div>
+        <div className="top-img-text">
+          Encuentra tu nuevo <div style={styles.data[title].style} className="top-letters">{styles.data[title].title}.</div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
