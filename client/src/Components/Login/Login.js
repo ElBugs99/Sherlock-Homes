ea88616b0cafe5fe5be3e25cb865ea136c4371b1
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-06 23:46:22 -0300

    .

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -7,7 +7,7 @@ import { LoginContext } from './LoginContext'
 export default function Login() {
 
 
-    //hacer una funcion que diga si los parentesis de apertura y cierre son correctos, se espera recibir un String
+//hacer una funcion que diga si los parentesis de apertura y cierre son correctos, se espera recibir un String
 
   //ejemplos de malos
   /* 
@@ -24,7 +24,89 @@ export default function Login() {
   //ordenar nombres que tienen numeros romanos en base al numero
   //ordernar las tallas, de mayor a menor, ordenar las tallas que se entregen, pueden no ser todas
 
-  const ordenarTallas = ( arrayDeTallas ) => {
+/*     const validarParentesis = (cadena) => {
+
+      const aperturas = [];
+
+      //const {name, phone, email} = clients.user;
+
+      const cadenaArray = [...cadena];
+
+      const parentesis = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+      }
+
+      console.log(cadenaArray);
+
+      for (let value of cadenaArray){
+
+          //verificar si es de cierre o apertura
+          if ( value in parentesis ) {
+            console.log(value + 'Es de apertura');
+            aperturas.push(value);
+          }else{
+            console.log(value + " es de cierre");
+            //if ( aperturas.length() === 0 ) return false;
+
+            let ultimaApertura = aperturas[aperturas.length - 1 ];
+
+            if (parentesis[ultimaApertura] === value) {
+              console.log(ultimaApertura + 'Le correponde ' + parentesis[ultimaApertura]);
+              aperturas.pop();
+            } else {
+              console.log('no le corresponde: ')
+              //return false;
+            }
+          }
+        
+      }
+      if (aperturas.length > 0){
+        console.log('cerraron todos los parantesis');
+      }
+      console.log(aperturas);
+    }
+
+
+  const verificarParentesis = (cadena) => {
+
+    const cadenaArray = [...cadena];
+    const aperturas = [];
+    const parentesis = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+    }
+
+    for (let value of cadenaArray) {
+      
+      //es de apertura
+      if (value in parentesis) {
+        console.log(value + 'es de apertura');
+        aperturas.push(value);
+      }else{
+        //es de cierre
+        console.log(value + 'es de cierre');
+        if ( aperturas.length === 0 ) return false;
+        
+        const ultimoParentesis = aperturas[ aperturas.length - 1];
+
+        if (value === parentesis[ultimoParentesis]) {
+          console.log('corresponden: ' + ultimoParentesis + value);
+          aperturas.pop();
+        }
+
+      }
+      
+    }
+
+    return aperturas.length === 0;
+
+  } *
+
+
+  /* const ordenarTallas = ( arrayDeTallas ) => {
 
       //Orden de las tallas
       const tallas =
@@ -47,7 +129,7 @@ export default function Login() {
         }
       }
       return ordenadas.reverse();
-  };
+  }; */
 
   const [loginUser, setLoginUser] = useState({
     "email": "",
