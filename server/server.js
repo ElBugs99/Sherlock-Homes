fd17273f72538e2ee2a9eb36b37db610d2328346
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-09 01:39:56 -0300

    removed comment

--- server/server.js
+++ server/server.js
@@ -52,28 +52,4 @@ app.listen(port, () => {
 });
 
 /* const url = 'https://www.portalinmobiliario.com/venta/casa/vina-del-mar-valparaiso#unapplied_filter_id%3Dneighborhood%26unapplied_filter_name%3DBarrios%26unapplied_value_id%3DTVhYUmXDsWFjYVRVeERRMVpKMFdRM1pHVTQ%26unapplied_value_name%3DRe%C3%B1aca%26unapplied_autoselect%3Dfalse';
-
-axios(url)
-    .then( response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const houses = [];
-
-        $('.ui-search-item__information', html).each(function() {
-            const info = $(this).text()
-            houses.push({
-                info
-            })
-            console.log(houses);
-        })
-    }).catch(error => console.log(error))
-
-app.use(cors()); // This allows all origins, you can configure it more specifically
-
-//set up api route
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne","userTwo","userThree","userFour"]})
-})
-
-app.listen(5000, () => {console.log("Server started at port 5000")})
  */
