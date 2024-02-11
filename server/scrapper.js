483e6c8c26bb1c3153a4ce71d79330c53243aa5a
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-10 23:19:32 -0300

    scrapping function

--- server/scrapper.js
+++ server/scrapper.js
@@ -1,2 +1,8 @@
 import puppeteer from "puppeteer";
 import fs from 'fs/promises';
+
+const getHouseData = async () => {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.portalinmobiliario.com/venta/casa/vina-del-mar-valparaiso#unapplied_filter_id%3Dneighborhood%26unapplied_filter_name%3DBarrios%26unapplied_value_id%3DTVhYUmXDsWFjYVRVeERRMVpKMFdRM1pHVTQ%26unapplied_value_name%3DRe%C3%B1aca%26unapplied_autoselect%3Dfalse');
