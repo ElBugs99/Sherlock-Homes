9c5297275e62e72120b736ae0041d1584918f13c
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-11 23:15:22 -0300

    export get houses function

---
+++ server/src/controllers/index.controller.js
@@ -0,0 +1,9 @@
+//aca van las funciones (callbacks)
+
+export const getHouses = (req, res) => {
+    res.send('houses');
+}
+
+/* export default {
+    getHouses
+}; */
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
 const router = Router();
+import { getHouses } from "../controllers/index.controller.js";
 
-router.get('/houses', (req, res) => {
-    res.send('houses');
-})
+router.get('/houses', getHouses)
 
 export default router;
