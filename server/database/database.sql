77626b0aeb4e35f8269b623dcefa6415ae4be7c7
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-12 22:23:48 -0300

    create house table

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE sherlockHomes;
+
+CREATE TABLE houses (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    priceString VARCHAR(255),
+    priceNumber VARCHAR(255), --guardado como texto
+    atributes TEXT,
+    location TEXT,
+    imageUrl TEXT,
+    url TEXT
+);
