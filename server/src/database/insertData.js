2b3b6e0b0d3517e6979255b42a17ba40364ad575
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-14 04:07:39 -0300

    insert data function

--- server/src/database/insertData.js
+++ server/src/database/insertData.js
@@ -1,2 +1,22 @@
 import fs from 'fs/promises';
 import { pool } from '../controllers/index.controller.js';
+
+async function insertData(filePath) {
+  const data = JSON.parse(await fs.readFile(filePath, 'utf8'));
+  //const client = await pool.connect();
+  try {
+    //await client.query('BEGIN');
+    for (const item of data) {
+      console.log(item);
+    }
+    //await client.query('COMMIT');
+  } catch (e) {
+    console.log('fallo la wea')
+    //await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    //client.release();
+  }
+}
+
+export default insertData;
