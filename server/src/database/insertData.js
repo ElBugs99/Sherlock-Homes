fe9b60b51acd68d44bf0893d8658b937e3e2c09f
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-15 20:08:15 -0300

    fixed path error

--- server/src/database/insertData.js
+++ server/src/database/insertData.js
@@ -3,8 +3,11 @@ import { pool } from '../controllers/index.controller.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, '..', '..', 'data', 'houseData.json');
+
+
 const hola = JSON.parse(await fs.readFile(filePath, 'utf8'));
 console.log(hola)
 
