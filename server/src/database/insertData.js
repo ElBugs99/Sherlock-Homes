81f0eb2459aa64bb6813984e1ac02e0b4fc607c7
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-15 00:21:57 -0300

    c

--- server/src/database/insertData.js
+++ server/src/database/insertData.js
@@ -1,10 +1,10 @@
 import fs from 'fs/promises';
 import { pool } from '../controllers/index.controller.js';
 import path from 'path';
-//const filePath = path.join(__dirname, '..', '..', 'data', 'houseData.json');
+const filePath = path.join(__dirname, '..', '..', 'data', 'houseData.json');
 console.log('hola')
-//const hola = JSON.parse(await fs.readFile('../../data/houseData.json', 'utf8'));
-//console.log(hola)
+const hola = JSON.parse(await fs.readFile(filePath, 'utf8'));
+console.log(hola)
 
 /* 
 async function insertData() {
