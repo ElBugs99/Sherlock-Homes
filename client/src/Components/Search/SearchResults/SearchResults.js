d32666d4e1e18509e3e906c26c7ae6d00c763ab1
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-16 23:27:09 -0300

    house data fetch resolved

--- client/src/Components/Search/SearchResults/SearchResults.js
+++ client/src/Components/Search/SearchResults/SearchResults.js
@@ -1,22 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeCard from '../../UI/HomeCard/HomeCard'
 
 export default function SearchResults() {
 
     const [houses , setHouses] = useState();
     const [loading , setLoading] = useState(true);
-    const [errro , setError] = useState(false);
+    const [error , setError] = useState(false);
 
     const fetchHouses = async() => {
         try {
-            const response = await fetch('http://localhost:3000/houses')
-            console.log(response);
+            const response = await fetch('http://localhost:3000/houses');
+            const data = await response.json();
+            console.log(data)
         } catch (error){
-
+            
         }
     }
 
-    fetchHouses();
+    useEffect(() => {
+        fetchHouses();
+    }, [])
 
     const flag = false;
 
