cad1f9de13d5020b52b67092bcfd3a4c0ffd70b3
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-16 21:07:08 -0300

    homeCard component props added

--- client/src/Components/UI/HomeCard/HomeCard.js
+++ client/src/Components/UI/HomeCard/HomeCard.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./homeCard.css";
 import img from "../../../assets/images/Faro_La_Serena_451.jpg";
 
-export default function HomeCard({ image, price }) {
+export default function HomeCard({ title, priceString, atributes, location, imageUrl, url }) {
   return (
     <div className="home-card">
       <div className="home-card-container">
         <div className="home-card-img-container">
-          <img className="home-card-img" src={img} alt="home" />
+          <img className="home-card-img" src={imageUrl} alt="home" />
         </div>
         <div className="home-card-info">
           <div className="">description</div>
