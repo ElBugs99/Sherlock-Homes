e9014d0e20741aae2e423863fe31a2aa16a3a618
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-20 03:41:44 -0300

    .

--- client/src/Components/Search/SearchResults/SearchResults.js
+++ client/src/Components/Search/SearchResults/SearchResults.js
@@ -50,7 +50,7 @@ export default function SearchResults() {
       })}
       
     </div>
-    <Pagination />
+    <Pagination postsPerPage={postsPerPage} currentPage={currentPage} postsLen={houses.length}/>
     </>
   )
 }
--- client/src/Components/UI/Pagination/Pagination.js
+++ client/src/Components/UI/Pagination/Pagination.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export default function Pagination({postsPerPage, currentPage}) {
+export default function Pagination({postsPerPage, currentPage, postsLen}) {
 
+    //cantidad de paginas = totalDePosts / postsPorPagina
     const pages = [];
 
   return (
     <div>
-      Hola
+      {postsPerPage} {currentPage} {postsLen}
     </div>
   )
 }
