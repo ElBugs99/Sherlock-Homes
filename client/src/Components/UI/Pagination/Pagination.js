5fe1296e8f0aef9b08c37c533520cc91742b4a6f
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-20 03:53:55 -0300

    total number of pages calculation

--- client/src/Components/UI/Pagination/Pagination.js
+++ client/src/Components/UI/Pagination/Pagination.js
@@ -5,9 +5,14 @@ export default function Pagination({postsPerPage, currentPage, postsLen}) {
     //cantidad de paginas = totalDePosts / postsPorPagina
     const pages = [];
 
+    for (let i = 1; i <= Math.ceil(postsLen/postsPerPage); i++) {
+        pages.push(i)
+    }
+    
+    console.log(pages);
   return (
     <div>
-      {postsPerPage} {currentPage} {postsLen}
+      {Math.ceil(postsLen/postsPerPage)}
     </div>
   )
 }
