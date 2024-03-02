559bc3ced31150096b41fd12ffa6ded7d3df9a3d
ElBugs99 <jpvaldivia99@gmail.com>
2024-03-02 01:22:05 -0300

    formmated file

--- client/src/Components/UI/SideBar/SideBar.js
+++ client/src/Components/UI/SideBar/SideBar.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import './sideBar.css';
+import React from "react";
+import "./sideBar.css";
 
 export default function SideBar() {
   return (
-    <div className='side-bar'>
+    <div className="side-bar">
       SideBar
-      <input type='checkbox' />
-      <input type='checkbox' />
-      <input type='checkbox' />
-      <input type='radio' value="Python"/>
-      <input type="radio" id="javascript" name="fav_language" value="JavaScript"></input>
+      <input type="checkbox" />
+      <input type="checkbox" />
+      <input type="checkbox" />
+      <input type="radio" value="Python" />
+      <input
+        type="radio"
+        id="javascript"
+        name="fav_language"
+        value="JavaScript"
+      ></input>
       <input
         type="range"
         name="price"
@@ -17,16 +22,18 @@ export default function SideBar() {
         min="50000"
         max="500000"
         step="100"
-        value="250000" />
-        <input type="datetime-local" name="datetime" id="datetime" />
-        <input type="month" name="month" id="month" />
-        <input
-          type="date"
-          name="myDate"
-          min="2013-06-01"
-          max="2013-08-31"
-          step="7"
-          id="myDate" />
+        value="250000"
+      />
+      <input type="datetime-local" name="datetime" id="datetime" />
+      <input type="month" name="month" id="month" />
+      <input
+        type="date"
+        name="myDate"
+        min="2013-06-01"
+        max="2013-08-31"
+        step="7"
+        id="myDate"
+      />
     </div>
-  )
+  );
 }
