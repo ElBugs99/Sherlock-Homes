449a4d34b81cae11b33ffec6195f66b489ca31a6
ElBugs99 <jpvaldivia99@gmail.com>
2024-03-05 15:01:55 -0300

    navBar props

--- client/src/Components/UI/NavBar/NavBar.js
+++ client/src/Components/UI/NavBar/NavBar.js
@@ -2,14 +2,19 @@ import React from 'react'
 import './navBar.css';
 import svgImage from '../../../assets/images/greenicon.svg';
 import { NavLink } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
 
-export default function NavBar() {
+export default function NavBar({searchHidden}) {
+
   return (
     <nav className='navBar'>
       <div className='nav-options-container'>
         <div className='nav-logo-container'>
           <div className='nav-logo'>Sherlock Homes</div>
           <img className='nav-icon' src={svgImage} alt="SVG" />
+        </div>
+        <div className={searchHidden? 'hidden' : ''}>
+          <SearchBar />
         </div>
         <div className='nav-options'>
           <NavLink to='/' className='nav-option'>Home</NavLink >
