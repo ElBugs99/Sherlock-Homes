d4c11081b714013590dabfc69335b0972376c27d
ElBugs99 <jpvaldivia99@gmail.com>
2024-03-05 14:39:53 -0300

    imported search icon

--- client/src/Components/UI/SearchBar/SearchBar.js
+++ client/src/Components/UI/SearchBar/SearchBar.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import "./searchBar.css"
+import search from "../../../assets/images/white-search-icon.svg";
 
 export default function SearchBar() {
   return (
     <div>
       <form className="top-form">
-          <input className="top-input" spellCheck="false" />
-          <button className="search-btn">
-            <img className="search-icon" alt="search" />
-          </button>
-        </form>
+        <input className="top-input" spellCheck="false" />
+        <button className="search-btn">
+          <img src={search} className="search-icon" alt="search" />
+        </button>
+      </form>
     </div>
   )
 }
