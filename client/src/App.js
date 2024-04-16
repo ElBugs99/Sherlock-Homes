5acaeba52cd2460eb80920c96e4d31d90cfea44d
ElBugs99 <jpvaldivia99@gmail.com>
2024-04-16 00:33:46 -0400

    app context provider in place

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,26 @@ import Login from "./Components/Login/Login";
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import NoMatch from "./Components/UI/NoMatch/NoMatch";
 import Search from "./Components/Search/Search";
+import { appContext } from "./appContext";
+import useApp from "./hooks/useApp";
 
 function App() {
+
+  const { houses } = useApp();
+
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </Router>
-    </div>
+    <appContext.Provider value={{ houses }}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </Router>
+      </div>
+    </appContext.Provider>
   );
 }
 
