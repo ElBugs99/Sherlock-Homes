d9f28b4ea9d9de58b213d96b4e60a54264cad78f
ElBugs99 <jpvaldivia99@gmail.com>
2024-04-19 13:03:40 -0400

    added navbar in info page

--- client/src/Components/InfoPage/InfoPage.js
+++ client/src/Components/InfoPage/InfoPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './infoPage.css'
 import houseKeys from '../../assets/images/houseKeys.jpg'
+import NavBar from '../UI/NavBar/NavBar'
 
 export default function InfoPage() {
     return (
         <div className='info-page'>
+
+            <NavBar searchHidden={true}/>
 
             <div className='info-page-row'>
                 <p>Texto ejemplo</p>
