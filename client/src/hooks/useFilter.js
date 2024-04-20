99a0d0699579f9b9143d9810b0c92ab001a8162a
ElBugs99 <jpvaldivia99@gmail.com>
2024-04-19 21:51:08 -0400

    fixes

--- client/src/hooks/useFilter.js
+++ client/src/hooks/useFilter.js
@@ -6,7 +6,7 @@ export default function useFilter() {
   const { houses, loading, error } = useContext(appContext);
   const [ SelectedFilters, setSelectedFilters ] = useState([]);
   const [ filteredItems, setFilteredItems ] = useState()
-  const filters = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l"];
+  const filters = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m"];
 
 
   return (
