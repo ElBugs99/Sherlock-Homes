ea5e361cb2c199df30f2e403bbb91725531809aa
GabrielQuezLab <gabrielquezlab@gmail.com>
2024-04-28 20:17:19 -0400

    primer front pag Nosostros, footer no visible, sin scrollbar

--- client/src/Components/InfoPage/InfoPage.js
+++ client/src/Components/InfoPage/InfoPage.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import './infoPage.css'
 import houseKeys from '../../assets/images/houseKeys.jpg'
+import CiudadOpaca from '../../assets/images/CiudadOpaca.jpg'
+import Footer from '../UI/Footer/Footer';
 import NavBar from '../UI/NavBar/NavBar'
 
 export default function InfoPage() {
     return (
         <div className='info-page'>
-
             <NavBar searchHidden={true}/>
-
-            <div className='info-page-row'>
-                <p>Texto ejemplo</p>
-                <img className='info-page-img' src={houseKeys} alt='keys' />
+            <div className='background' style={{ backgroundImage: `url(${CiudadOpaca})` }}>
+                <div className='logo'>
+                    <h1>Sherlock Homes</h1>
+                </div>
+                <div className='info-page-row'>
+                    <div className='enunciado'>
+                    <p>Somos una plataforma que quiere ayudar a quienes buscan un hogar, ante la problematica que significa 
+                        la gran variedad de páginas de anuncios inmobiliarios que hay en el mercado
+                        intentando unificar los avisos de estas en una sola página, entregando datos relevantes al usuario y 
+                        redireccionandolo a la página de origen del aviso que le interesa
+                        </p>                        
+                    </div>          
+                    <img className='info-page-img' src={houseKeys} alt='keys' />
+                </div>
+                <Footer/>
             </div>
-
-            <div>
-                InfoPage
-            </div>
-
         </div>
+        
     )
 }
