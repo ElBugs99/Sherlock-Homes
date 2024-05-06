c64a1ef661da9771cf8b3391124ee81bb831d952
GabrielQuezLab <gabrielquezlab@gmail.com>
2024-05-06 01:29:20 -0400

    Arreglos al diseño de la PageInfo y correcciónes al footer

--- client/src/Components/InfoPage/InfoPage.js
+++ client/src/Components/InfoPage/InfoPage.js
@@ -1,31 +1,38 @@
 import React from 'react'
 import './infoPage.css'
 import houseKeys from '../../assets/images/houseKeys.jpg'
-import CiudadOpaca from '../../assets/images/CiudadOpaca.jpg'
+import Fondo from '../../assets/images/Fondo.jpg'
 import Footer from '../UI/Footer/Footer';
 import NavBar from '../UI/NavBar/NavBar'
 
 export default function InfoPage() {
     return (
         <div className='info-page'>
-            <NavBar searchHidden={true}/>
-            <div className='background' style={{ backgroundImage: `url(${CiudadOpaca})` }}>
-                <div className='logo'>
-                    <h1>Sherlock Homes</h1>
-                </div>
-                <div className='info-page-row'>
+            <div className='navbar'>
+                <NavBar searchHidden={true}/>
+            </div>
+            <div className='main-content'>
+                <div className='background' style={{ backgroundImage: `url(${Fondo})` }}>
+                    <div className='logo'>
+                        <h1>Sherlock Homes</h1>
+                    </div>
+                    <div className='info-page-row'>
                     <div className='enunciado'>
-                    <p>Somos una plataforma que quiere ayudar a quienes buscan un hogar, ante la problematica que significa 
+                    <h3>Somos una plataforma que quiere ayudar a quienes buscan un hogar, ante la problematica que significa 
                         la gran variedad de páginas de anuncios inmobiliarios que hay en el mercado
                         intentando unificar los avisos de estas en una sola página, entregando datos relevantes al usuario y 
                         redireccionandolo a la página de origen del aviso que le interesa
-                        </p>                        
+                        </h3>                        
+                    </div>
+                    <div className='img-1'>
+                        <img className='info-page-img' src={houseKeys} alt='keys' />
                     </div>          
-                    <img className='info-page-img' src={houseKeys} alt='keys' />
                 </div>
-                <Footer/>
+            </div>
+                <div className='footer'>
+                    <Footer/>
+                </div>
             </div>
         </div>
-        
-    )
+    );
 }
--- client/src/Components/UI/Footer/Footer.js
+++ client/src/Components/UI/Footer/Footer.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import './footer.css'
+import fbI from '../../../assets/images/FacebookIcon.png';
+import igI from '../../../assets/images/InstagramIcon.png';
+import xI from '../../../assets/images/XIcon.png';
+import logo from '../../../assets/images/LogoShH.png';
 
 export default function Footer() {
   return (
     <div className='pageFooter'>
-      ® 2023, SherlockHomes SpA.
-      <div>Todos los derechos reservados.</div>
-      <div className='footer__links'>link</div>
+      <div className='grupo-1'>
+        <div className='box'>
+          <figure>
+            <a href="#">
+            <img className='logoimg' src={logo} alt="Sherlock Homes" />     
+            </a>
+          </figure>
+        </div>
+        <div className='box'>
+          <h2>SOBRE NOSOTROS</h2>
+          <p>
+            Somos una página que se dedica a recopilar anuncios inmoviliarios
+          </p>
+          <p>
+            Te ayudaremos a encontrar tu nuevo hogar
+          </p>
+        </div>
+        <div className='box'>
+          <h2>SIGUENOS EN:</h2>
+          <div className="red-social">
+            <a href="#" className="">
+            <img className='rrss-icon' src={fbI} alt="Facebook" />  
+            </a>
+            <a href="#">
+            <img className='rrss-icon' src={igI} alt="Instagram" />                
+            </a>
+            <a href="#">
+            <img className='rrss-icon' src={xI} alt="X" />                
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='grupo-2'>
+        <b>® 2023, SherlockHomes SpA.</b>
+        <div>Todos los derechos reservados.</div>
+      </div>
     </div>
   )
 }
