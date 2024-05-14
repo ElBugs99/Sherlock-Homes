4ccb7d61d0a83342a8939096a9c838a1030d7fc4
ElBugs99 <jpvaldivia99@gmail.com>
2024-05-14 01:46:55 -0400

    number utils modified

--- client/src/utils/numberUtils.js
+++ client/src/utils/numberUtils.js
@@ -1,8 +1,7 @@
 export function addDotsToNumber(number) {
-    // Convert number to string
+
     let numberStr = number.toString();
-  
-    // Add dots every three characters from the right
+
     let result = '';
     let count = 0;
     for (let i = numberStr.length - 1; i >= 0; i--) {
