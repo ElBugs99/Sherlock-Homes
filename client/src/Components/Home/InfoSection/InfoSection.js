8ca1874bb6a60b473ed2d7bb1f9f4d85b1f2979d
ElBugs99 <jpvaldivia99@gmail.com>
2024-05-17 22:03:10 -0400

    feature card created and infoSection refactor

--- client/src/Components/Home/InfoSection/InfoSection.js
+++ client/src/Components/Home/InfoSection/InfoSection.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import './infoSection.css'
-import casa1 from '../../../assets/images/Ehcasa1.jpeg'
-import { FaHeart } from 'react-icons/fa';
-import { FaShare } from "react-icons/fa";
-import { PiToiletLight } from "react-icons/pi";
-import { IoBedOutline } from "react-icons/io5";
-import { TbMeterSquare } from "react-icons/tb";
+import FeaturedCard from '../../UI/FeaturedCard/FeaturedCard';
 
 
 export default function InfoSection() {
@@ -15,185 +10,18 @@ export default function InfoSection() {
 
     <section className='article'>
       <div className='infoSection'>
-        <div className='card'>
-          <h3 className='Venta'>Oferta Destacada</h3>
 
-          <div className='iconos'>
-            <FaHeart className='like' />
-            <FaShare className='Compartir' />
-          </div>
-
-
-          <center>
-            <img className='casas' src={casa1} alt='Los Alemendros' />
-          </center>
-          <p className='descripcion'>Los Almendros. Reñaca. Propiedad con 1680 m2 de terreno.</p>
-          <h3>UF 19.990</h3>
-
-          <p className='cantidad'>
-            <PiToiletLight /> 3 Baños
-          </p>
-          <p className='cantidad'>
-            <IoBedOutline /> 4 Dormitorios
-          </p>
-          <p className='cantidad'>
-            <TbMeterSquare /> 1680 m2
-          </p>
-
-          <div className='botonOferta'>
-            <a href='*'>
-              <button className='btn-oferta'>Ir a la oferta</button>
-            </a>
-            <button className='btn-comparar'>Comparar</button>
-          </div>
-
-        </div>
-        <div className='card'>
-          <h3 className='Venta'>Oferta Destacada</h3>
-
-          <div className='iconos'>
-            <FaHeart className='like' />
-            <FaShare className='Compartir' />
-          </div>
-
-
-          <center>
-            <img className='casas' src={casa1} alt='Los Alemendros' />
-          </center>
-          <p className='descripcion'>Los Almendros. Reñaca. Propiedad con 1680 m2 de terreno.</p>
-          <h3>UF 19.990</h3>
-
-          <p className='cantidad'>
-            <PiToiletLight /> 3 Baños
-          </p>
-          <p className='cantidad'>
-            <IoBedOutline /> 4 Dormitorios
-          </p>
-          <p className='cantidad'>
-            <TbMeterSquare /> 1680 m2
-          </p>
-          <div className='botonOferta'>
-            <a href='*'>
-              <button className='btn-oferta'>Ir a la oferta</button>
-            </a>
-            <button className='btn-comparar'>Comparar</button>
-          </div>
-
-
-
-        </div>
-        <div className='card'>
-          <h3 className='Venta'>Oferta Destacada</h3>
-
-          <div className='iconos'>
-            <FaHeart className='like' />
-            <FaShare className='Compartir' />
-          </div>
-
-          <div className='info-casa'>
-            <center>
-              <img className='casas' src={casa1} alt='Los Alemendros' />
-            </center>
-            <p className='descripcion'>Los Almendros. Reñaca. Propiedad con 1680 m2 de terreno.</p>
-            <h3>UF 19.990</h3>
-
-            <p className='cantidad'>
-              <PiToiletLight /> 3 Baños
-            </p>
-            <p className='cantidad'>
-              <IoBedOutline /> 4 Dormitorios
-            </p>
-            <p className='cantidad'>
-              <TbMeterSquare /> 1680 m2
-            </p>
-          </div>
-
-          <div className='botonOferta'>
-            <a href='*'>
-              <button className='btn-oferta'>Ir a la oferta</button>
-            </a>
-            <button className='btn-comparar'>Comparar</button>
-          </div>
-
+        <div className='feature-element'>
+          <FeaturedCard />
         </div>
 
-        {/* <div className='card'> 
-      <h3 className='Venta'>Oferta Destacada</h3>
-
-      <div className='iconos'>
-      <FaHeart className='like'  />
-      <FaShare className='Compartir'/>
-      </div>
+        <div className='feature-element'>
+          <FeaturedCard />
+        </div>
       
-      
-          <center>
-          <img className='casas' src={casa1} alt='Los Alemendros' />
-          </center>
-            <p className='descripcion'>Los Almendros. Reñaca. Propiedad con 1680 m2 de terreno.</p>
-            <h3>UF 19.990</h3>
-
-            <p className='cantidad'>
-            <PiToiletLight/> 3 Baños
-            </p>
-            <p className='cantidad'>
-            <IoBedOutline/> 4 Dormitorios
-            </p>
-            <p className='cantidad'>
-            <TbMeterSquare/> 1680 m2
-            </p>
-
-           
-            
-      </div> */}
-
-        {/* <div className='card'> 
-      <h3 className='Venta'>Oferta Destacada</h3>
-      <div className='iconos'>
-      <FaHeart className='like'  />
-      <FaShare className='Compartir'/>
-      </div>
-          <center>
-          <img className='casas' src={casa1} alt='Los Alemendros' />
-          </center>
-            <p className='descripcion'>Los Almendros. Reñaca. Propiedad con 1680 m2 de terreno.</p>
-            <h3>UF 19.990</h3>
-
-            <p className='cantidad'>
-            <PiToiletLight/> 3 Baños
-            </p>
-            <p className='cantidad'>
-            <IoBedOutline/> 4 Dormitorios
-            </p>
-            <p className='cantidad'>
-            <TbMeterSquare/> 1680 m2
-            </p>
-
-      </div> */}
-
-        {/* <div className='card'> 
-      <h3 className='Venta'>Oferta Destacada</h3>
-
-      <div className='iconos'>
-      <FaHeart className='like'  />
-      <FaShare className='Compartir'/>
-      </div>
-          <center>
-          <img className='casas' src={casa1} alt='Los Alemendros' />
-          </center>
-            <p className='descripcion'>Los Almendros. Reñaca. Propiedad con 1680 m2 de terreno.</p>
-            <h3>UF 19.990</h3>
-
-            <p className='cantidad'>
-            <PiToiletLight/> 3 Baños
-            </p>
-            <p className='cantidad'>
-            <IoBedOutline/> 4 Dormitorios
-            </p>
-            <p className='cantidad'>
-            <TbMeterSquare/> 1680 m2
-            </p>
-      </div> */}
-
+        <div className='feature-element'>
+          <FeaturedCard />
+        </div>
 
       </div>
     </section>
