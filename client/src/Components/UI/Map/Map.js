70379956b752e5947e01be0c77168ef87caf2601
OneRonnin97 <ronin.dlsr@gmail.com>
2024-05-22 12:52:31 -0400

    se agrega mapa

---
+++ client/src/Components/UI/Map/Map.js
@@ -0,0 +1,35 @@
+import React from "react";
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap(){
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627
+    },
+    zoom: 11,
+    size:{
+        width: 500,
+        height: 650
+    }
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div className="map" style={{ height: '40vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  );
+}
