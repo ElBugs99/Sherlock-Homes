7f26bd317c7aef5ff3d2f2da325e76c07b75efab
ElBugs99 <jpvaldivia99@gmail.com>
2024-05-22 03:20:49 -0400

    profile page basic layout

--- client/src/Components/ProfilePage/ProfilePage.js
+++ client/src/Components/ProfilePage/ProfilePage.js
@@ -5,21 +5,33 @@ import UserCard from '../UI/UserCard/UserCard';
 import Footer from '../UI/Footer/Footer';
 
 export default function ProfilePage() {
-  return (
-    <div className='profile-page-container'>
-        <NavBar searchHidden={true} />
-        <div className='profile-page'>
-            <div className='profile-page-left'>
-                <UserCard />
+    return (
+        <div className='profile-page-container'>
+            <NavBar searchHidden={true} />
+            <div className='profile-page'>
+                <div className='profile-page-left'>
+                    <UserCard />
+                </div>
+                <div className='profile-page-center'>
+                    <div className='profile-center-top'>
+                        <div className='profile-info-box'>
+                            Favoritos
+                        </div>
+                        <div className='profile-info-box'>
+                            Comentarios
+                        </div>
+                        <div className='profile-info-box'>
+                            Publicaciones
+                        </div>
+                    </div>
+
+                    center
+                </div>
+                <div className='profile-page-right'>
+                    right
+                </div>
             </div>
-            <div className='profile-page-center'>
-                center
-            </div>
-            <div className='profile-page-right'>
-                right
-            </div>
+            <Footer />
         </div>
-        <Footer />
-    </div>
-  )
+    )
 }
