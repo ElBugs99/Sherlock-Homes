de7e5ea085abab8457790b4a61d374965905ba5c
ElBugs99 <jpvaldivia99@gmail.com>
2024-05-24 04:16:54 -0400

    user profile images

--- client/src/Components/UI/UserCard/UserCard.js
+++ client/src/Components/UI/UserCard/UserCard.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './userCard.css';
 import Lottie from 'react-lottie';
 import useApp from '../../../hooks/useApp';
 import animationData from '../../../assets/images/background.json';
+
+//user images
 import perro from '../../../assets/images/perro.png';
+import perro2 from '../../../assets/images/perro2.png';
+import anaconda from '../../../assets/images/anaconda.png';
+import ganesha from '../../../assets/images/ganesha.png';
+import jaguar from '../../../assets/images/jaguar.png';
+import lobo from '../../../assets/images/lobo.png';
+import pandaBear from '../../../assets/images/panda-bear.png';
+import parrot from '../../../assets/images/parrot.png';
+import walrus from '../../../assets/images/walrus.png';
+
+const userImages = [perro, perro2, anaconda, ganesha, jaguar, lobo, pandaBear, parrot, walrus];
 
 export default function UserCard() {
   const { user } = useApp();
+  const [randomImage, setRandomImage] = useState('');
+
+  useEffect(() => {
+    // Select a random image from the userImages array
+    const randomIndex = Math.floor(Math.random() * userImages.length);
+    setRandomImage(userImages[randomIndex]);
+  }, []);
 
   const defaultOptions = {
     loop: true,
@@ -28,7 +47,7 @@ export default function UserCard() {
         </div>
         <div className='user-content'>
           <div className='user-image-container'>
-            <img src={perro} alt='User Profile' className='user-image' />
+            <img src={randomImage} alt='User Profile' className='user-image' />
           </div>
           <div className='user-info'>
             <h2 className='user-name'>{user?.username || 'User'}</h2>
