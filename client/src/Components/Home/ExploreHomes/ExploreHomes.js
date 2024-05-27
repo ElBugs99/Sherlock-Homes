1ebe4112af509d336ff11555fdd9fe1b35c14e91
ElBugs99 <jpvaldivia99@gmail.com>
2024-05-26 20:39:42 -0400

    explore city css changed

--- client/src/Components/Home/ExploreHomes/ExploreHomes.js
+++ client/src/Components/Home/ExploreHomes/ExploreHomes.js
@@ -1,11 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import './exploreHomes.css';
 import { appContext } from "../../../appContext";
 import ExploreCityCard from '../../UI/ExploreCityCard/ExploreCityCard';
 import vina from '../../../assets/images/vina-noche.jpg';
-import santiago from '../../../assets/images/santiago.jpg';
-import serena from '../../../assets/images/Faro_La_Serena_451.jpg';
-import valdivia from '../../../assets/images/valdivia.jpg';
+import valparaiso from '../../../assets/images/Valparaíso.jpg';
+import quilpue from '../../../assets/images/Quilpue.jpg';
+import villa from '../../../assets/images/molino2.png';
 
 export default function ExploreHomes() {
 
@@ -24,9 +24,9 @@ export default function ExploreHomes() {
         </div>
         <div className='explore-cities apareciendo'>
             <ExploreCityCard cityName={'Viña del Mar'} listings={50} image={vina} />
-            <ExploreCityCard cityName={'Santiago'} listings={50} image={santiago}/>
-            <ExploreCityCard cityName={'La Serena'} listings={50} image={serena}/>
-            <ExploreCityCard cityName={'Valdivia'} listings={50} image={valdivia}/>
+            <ExploreCityCard cityName={'Valparaíso'} listings={50} image={valparaiso}/>
+            <ExploreCityCard cityName={'Quilpué'} listings={50} image={quilpue}/>
+            <ExploreCityCard cityName={'Villa alemana'} listings={50} image={villa}/>
         </div>
       </div>
     </div>
--- client/src/Components/UI/ExploreCityCard/ExploreCityCard.js
+++ client/src/Components/UI/ExploreCityCard/ExploreCityCard.js
@@ -5,8 +5,10 @@ export default function ExploreCityCard({cityName, listings, image}) {
   return (
     <div className='Explore-city-card'>
       <img className='city-card-img' src={image} alt='city'/>
-      <div className='city-card-title'>{cityName}</div>
-      <div className='city-card-info'>Listados: {listings}</div>
+      <div className='city-card-letters'>
+        <div className='city-card-title'>{cityName}</div>
+        <div className='city-card-info'>Listados: {listings}</div>
+      </div>
     </div>
   )
 }
