e7ba71855a599b754c95756ebcf508e38fa784ec
OneRonnin97 <ronin.dlsr@gmail.com>
2024-05-27 13:04:27 -0400

    Se agrega estilo y street view a google maps y se modifica el css de property page

--- client/src/Components/UI/Map/Map.js
+++ client/src/Components/UI/Map/Map.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleMapReact from 'google-map-react';
-import { FaMapPin } from "react-icons/fa";
+import { FaMapPin, FaStreetView } from "react-icons/fa";
 
-const AnyReactComponent = ({ text }) =>
+const Marker= ({ text }) =>
    <div>
-    <FaMapPin />
+    <FaMapPin style={{color:'green' ,fontSize:'27px'}}/>
 
     {text}
    
    </div>;
+   const StreetViewIcon = ({ onClick }) => (
+    <div onClick={onClick}>
+      <FaStreetView style={{ color: 'blue', fontSize: '27px', cursor: 'pointer' }} />
+    </div>
+  );
 
 export default function SimpleMap({lat,lng}){
+
+  
+
 
   const defaultProps = {
     center: {
@@ -25,19 +33,144 @@ export default function SimpleMap({lat,lng}){
     streetViewControl: true
   };
 
+  const handleApiLoaded = (map, maps) => {
+    // Use map and maps objects to add additional functionalities
+  };
+
+
+
   console.log("latitud mapa" , lat,lng)
   return (
-    // Important! Always set the container height explicitly
+    
     <div className="map" style={{ height: '40vh', width: '100%' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyAEDMgJRUtJOdsBK45_U_-ytsFPfpZGYas" }}
         defaultCenter={defaultProps.center}
+       
         defaultZoom={defaultProps.zoom}
         layerTypes={['TransitLayer']}
+        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+        options={{
+          streetViewControl:true,
+          styles: [
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#FFBB00"
+                    },
+                    {
+                        "saturation": 43.400000000000006
+                    },
+                    {
+                        "lightness": 37.599999999999994
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#00FF6A"
+                    },
+                    {
+                        "saturation": -1.0989010989011234
+                    },
+                    {
+                        "lightness": 11.200000000000017
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#FFC200"
+                    },
+                    {
+                        "saturation": -61.8
+                    },
+                    {
+                        "lightness": 45.599999999999994
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#FF0300"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 51.19999999999999
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#FF0300"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 52
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#0078FF"
+                    },
+                    {
+                        "saturation": -13.200000000000003
+                    },
+                    {
+                        "lightness": 2.4000000000000057
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            }
+        ]
+        }}
+       
       >
-        <AnyReactComponent
+
+        
+        <Marker
           lat={lat}
           lng={lng}
+        
           
         />
       </GoogleMapReact>
