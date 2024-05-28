1cce2c87d316da899839f8835bc8fa7b2e3b85b2
ElBugs99 <jpvaldivia99@gmail.com>
2024-05-27 22:47:04 -0400

    property page css upgrade

--- client/src/Components/UI/Graffics/Graficos.js
+++ client/src/Components/UI/Graffics/Graficos.js
@@ -1,62 +1,44 @@
 import React from "react";
-import "./graficos.css"
+import "./graficos.css";
 import { PieChart } from '@mui/x-charts/PieChart';
-import { BarChart } from '@mui/x-charts/BarChart';
 
-export default function Graficos({bed,bath,mts,valor}){
-    const data = [
-        { id: 0, value: Number(valor), label: 'Valor CLP Propiedad actual' },
-        { id: 1, value: 400000000, label: 'Promedio Valor CLP Viña del Mar' }
-        
-      ];
-      const colors =['#edc949','#ff9da7']
-      
-      
-      const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-      const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-      const xLabels = [
-        'Page A',
-        'Page B',
-        'Page C',
-        'Page D',
-        'Page E',
-        'Page F',
-        'Page G',
-      ];
+export default function Graficos({ bed, bath, mts, valor }) {
+  const data = [
+    { id: 0, value: Number(valor), label: 'Valor CLP Propiedad actual' },
+    { id: 1, value: 400000000, label: 'Promedio Valor CLP Viña del Mar' }
+  ];
+  
+  const colors = ['#fefae0', '#3b6978', '#3c6e71', '#e68459'];
 
-return(
-
-    
-    
+  return (
     <PieChart
-    
       series={[
         {
           data,
-          highlightScope: { faded: 'global', highlighted: 'item' },
-          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+          highlightScope: { 
+            faded: 'global', 
+            highlighted: 'item' 
+          },
+          faded: { 
+            innerRadius: 30, 
+            additionalRadius: 0, 
+            color: 'gray'
+          },
+          highlighted: { 
+            additionalRadius: 10,
+            innerRadius: 30
+          },
+          color: colors
         },
       ]}
       height={430}
-      
       margin={{
         left: 0,
         right: 280,
-        
-        bottom: 10, 
+        bottom: 10,
         top: 130,
       }}
-      
       colors={colors}
-      
-      
     />
-        
-    
-
-
-
-)
-
-
+  );
 }
