97c4693c8bdd52aa4b185b796e575e70c391660b
GabrielQuezLab <gabrielquezlab@gmail.com>
2024-05-28 19:35:53 -0400

    modificaciones de diseño en la página nosotros

--- client/src/Components/InfoPage/InfoPage.js
+++ client/src/Components/InfoPage/InfoPage.js
@@ -4,35 +4,45 @@ import houseKeys from '../../assets/images/houseKeys.jpg'
 import Fondo from '../../assets/images/Fondo.jpg'
 import Footer from '../UI/Footer/Footer';
 import NavBar from '../UI/NavBar/NavBar'
+import { Fade } from "react-awesome-reveal";
 
 export default function InfoPage() {
     return (
-        <div className='info-page'>
-            <div className='navbar'>
-                <NavBar searchHidden={true}/>
+        <Fade>
+        <div className="terminoscondiciones">
+            <NavBar searchHidden={true} />
+
+            <div className='header'>
+                <div className='encabezado'>
+                    <h2 className="titulo">Sherlock Homes</h2>
+                </div>
             </div>
-            <div className='main-content'>
-                <div className='background' style={{ backgroundImage: `url(${Fondo})` }}>
-                    <div className='logo'>
-                        <h1>Sherlock Homes</h1>
-                    </div>
-                    <div className='info-page-row'>
-                    <div className='enunciado'>
-                    <p>Somos una plataforma que quiere ayudar a quienes buscan un hogar, ante la problematica que significa 
+            <div className='background' style={{ backgroundImage: `url(${Fondo})` }}></div>
+            <div className='info-content'>
+            <div className='info'>
+                <h3 className='sub-seccion'>
+                    ¿QUIENES SOMOS?
+                </h3>
+                <h1>
+                Somos una plataforma sin fines de lucro que quiere ayudar a quienes buscan un hogar, ante la problematica que significa 
                         la gran variedad de páginas de anuncios inmobiliarios que hay en el mercado
-                        intentando unificar los avisos de estas en una sola página, entregando datos relevantes al usuario y 
-                        redireccionandolo a la página de origen del aviso que le interesa
-                        </p>                        
-                    </div>
-                    <div className='img-1'>
-                        <img className='info-page-img' src={houseKeys} alt='keys' />
-                    </div>          
-                </div>
+                        intentamos unificar los avisos de estas en una sola página, entregando datos relevantes al usuario
+                        redireccionandolo a la página de origen del aviso que le interesa además de entregar fuentes de información en asuntos importantes del mercado inmobiliario
+                </h1>
+
+                <img className='imgKeys' src={houseKeys}  />
+
+                <h1 className='h1vamos'>
+                Permitenos acompañarte y apoyarte mientras buscas en nuestro gran catalogo de recopilación tu próximo hogar
+                </h1>
             </div>
-                <div className='footer'>
-                    <Footer/>
-                </div>
             </div>
+            <Footer />
         </div>
+        </Fade>
     );
 }
+
+
+
+
