51c3e5b151151f2f3425b624183d72f844e7e4bf
ElBugs99 <jpvaldivia99@gmail.com>
2024-05-28 03:38:56 -0400

    calculator styling

--- client/src/Components/UI/Calculator/Calculator.js
+++ client/src/Components/UI/Calculator/Calculator.js
@@ -1,55 +1,51 @@
 
-import React ,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './calculator.css'
 
 import { useParams } from 'react-router-dom';
 
 
-export default function Calculator({valor}){
-    const [propertyPrice, setPropertyPrice] = useState('');
-    const [downPayment, setDownPayment] = useState('');
-    const [interestRate, setInterestRate] = useState('');
-    const [loanTerm, setLoanTerm] = useState('');
-    const [monthlyPayment, setMonthlyPayment] = useState(null);
-    const [totalInterest, setTotalInterest] = useState(null);
-    const [totalCost, setTotalCost] = useState(null);
-  
-    const calculateMortgage = () => {
-      const loanAmount = propertyPrice - downPayment;
-      const monthlyInterestRate = (interestRate / 100) / 12;
-      const numberOfPayments = loanTerm * 12;
-  
-      const monthlyPayment = 
-        (loanAmount * monthlyInterestRate) / 
-        (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
-      
-      const totalCost = monthlyPayment * numberOfPayments;
-      const totalInterest = totalCost - loanAmount;
-  
-      setMonthlyPayment(monthlyPayment.toFixed(2));
-      setTotalInterest(totalInterest.toFixed(2));
-      setTotalCost(totalCost.toFixed(2));
-    };
+export default function Calculator({ valor }) {
+  const [propertyPrice, setPropertyPrice] = useState('');
+  const [downPayment, setDownPayment] = useState('');
+  const [interestRate, setInterestRate] = useState('');
+  const [loanTerm, setLoanTerm] = useState('');
+  const [monthlyPayment, setMonthlyPayment] = useState(null);
+  const [totalInterest, setTotalInterest] = useState(null);
+  const [totalCost, setTotalCost] = useState(null);
 
-    
+  const calculateMortgage = () => {
+    const loanAmount = propertyPrice - downPayment;
+    const monthlyInterestRate = (interestRate / 100) / 12;
+    const numberOfPayments = loanTerm * 12;
 
+    const monthlyPayment =
+      (loanAmount * monthlyInterestRate) /
+      (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
 
-    
-    return(
-        <div className="calculadora">
+    const totalCost = monthlyPayment * numberOfPayments;
+    const totalInterest = totalCost - loanAmount;
+
+    setMonthlyPayment(monthlyPayment.toFixed(2));
+    setTotalInterest(totalInterest.toFixed(2));
+    setTotalCost(totalCost.toFixed(2));
+  };
+
+  return (
+    <div className="calculadora">
       <h3 className='titulo-calc'>Calculadora de Hipoteca</h3>
       <div className="form-group">
         <label className='label-calc' htmlFor="propertyPrice">Precio de la propiedad:</label>
         <div className='input-calc'>
-        <input
-        type="number"
-        id="propertyPrice"
-        value={valor}
-        onChange={(e) => setPropertyPrice(e.target.value)}
-        
-        />  
+          <input
+            type="number"
+            id="propertyPrice"
+            value={valor}
+            onChange={(e) => setPropertyPrice(e.target.value)}
+            className='calc-input'
+          />
         </div>
-        
+
       </div>
       <div className="form-group">
         <label className='label-calc' htmlFor="downPayment">Pago inicial:</label>
@@ -59,22 +55,24 @@ export default function Calculator({valor}){
             id="downPayment"
             value={downPayment}
             onChange={(e) => setDownPayment(e.target.value)}
-            
+            className='calc-input'
+
           />
         </div>
-        
+
       </div>
       <div className="form-group">
         <label className='label-calc' htmlFor="interestRate">Tasa de interés anual (%):</label>
-      <div className='input-calc'>
-        <input
-          type="number"
-          id="interestRate"
-          step="0.01"
-          value={interestRate}
-          onChange={(e) => setInterestRate(e.target.value)}
-        /></div>
-        
+        <div className='input-calc'>
+          <input
+            type="number"
+            id="interestRate"
+            step="0.01"
+            value={interestRate}
+            onChange={(e) => setInterestRate(e.target.value)}
+            className='calc-input'
+          /></div>
+
       </div>
       <div className="form-group">
         <label className='label-calc' htmlFor="loanTerm">Plazo del préstamo (años):</label>
@@ -84,9 +82,10 @@ export default function Calculator({valor}){
             id="loanTerm"
             value={loanTerm}
             onChange={(e) => setLoanTerm(e.target.value)}
+            className='calc-input'
           />
         </div>
-       
+
       </div>
       <button className='boton-Calc' onClick={calculateMortgage}>Calcular</button>
       {monthlyPayment && (
@@ -98,8 +97,5 @@ export default function Calculator({valor}){
       )}
     </div>
 
-
-
-
-    )
+  )
 }
