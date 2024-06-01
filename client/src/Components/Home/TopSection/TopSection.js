5f89aab5dd9a27ae140496e7287df916d850e5bf
ElBugs99 <jpvaldivia99@gmail.com>
2024-06-01 05:27:46 -0400

    changed homepage video

--- client/src/Components/Home/TopSection/TopSection.js
+++ client/src/Components/Home/TopSection/TopSection.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./topSection.css";
-import homeVideo from "../../../assets/videos/home-video.mp4";
+import homeVideo from "../../../assets/videos/edificios1.mp4";
 import search from "../../../assets/images/white-search-icon.svg";
 import DropDown from '../../UI/DropDown/DropDown';
 import SearchBar from "../../UI/SearchBar/SearchBar";
