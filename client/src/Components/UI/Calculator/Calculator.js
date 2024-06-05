3f310f79ae8dac2db83df7bd150bcfd79fb6a763
OneRonnin97 <ronin.dlsr@gmail.com>
2024-06-05 11:21:01 -0400

    se termina la calculadora de credito hipotecario

--- client/src/Components/UI/Calculator/Calculator.js
+++ client/src/Components/UI/Calculator/Calculator.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import './calculator.css'
 import { addDotsToNumber } from '../../../utils/numberUtils';
 import { useParams } from 'react-router-dom';
+import { FaInfoCircle } from "react-icons/fa";
 
 
 export default function Calculator({ valor }) {
@@ -41,6 +42,14 @@ export default function Calculator({ valor }) {
     setTotalCost(totalCost.toFixed(2));
   };
 
+  const clearInputs = () => {
+    setDownPayment('');
+    setInterestRate('');
+    setLoanTerm('');
+    setMonthlyPayment(null);
+    setTotalInterest(null);
+    setTotalCost(null);
+  };
     
 
   return (
@@ -60,7 +69,11 @@ export default function Calculator({ valor }) {
 
       </div>
       <div className="form-group">
-        <label className='label-calc' htmlFor="downPayment">Pago inicial:</label>
+        <label className='label-calc' htmlFor="downPayment">Pago inicial (CLP):
+        <span className="info-icon" title="Es el monto del valor de la propiedad que NO esta cubierto por el credito">
+        <FaInfoCircle />
+        </span>
+        </label>
         <div className='input-calc'>
           <input
             type="number"
@@ -74,7 +87,11 @@ export default function Calculator({ valor }) {
 
       </div>
       <div className="form-group">
-        <label className='label-calc' htmlFor="interestRate">Tasa de interés anual (%):</label>
+        <label className='label-calc' htmlFor="interestRate">Tasa de interés anual (%):
+        <span className="info-icon" title="La tasa de interes anual depende del banco en el cual solicitara el credito hipotecario.">
+        <FaInfoCircle />
+        </span>
+        </label>
         <div className='input-calc'>
           <input
             type="number"
@@ -87,8 +104,12 @@ export default function Calculator({ valor }) {
 
       </div>
       <div className="form-group">
-        <label className='label-calc' htmlFor="loanTerm">Plazo del préstamo (años):</label>
-        <div>
+        <label className='label-calc' htmlFor="loanTerm">Plazo del préstamo (años):
+        <span className="info-icon" title="Tiempo que se estara pagando el credito, generalmente en cuotas mensuales">
+        <FaInfoCircle />
+        </span>
+        </label>
+        <div className='input-calc'>
           <input
             type="number"
             id="loanTerm"
@@ -99,12 +120,21 @@ export default function Calculator({ valor }) {
         </div>
 
       </div>
+      <div className='boton-calc-aling'>
       <button className='boton-Calc' onClick={calculateMortgage}>Calcular</button>
+      <button className='boton-Calc-borrar' onClick={clearInputs}>Borrar</button>
+      </div>
+      
       {monthlyPayment && (
         <div id="results">
           <p>Pago mensual: ${addDotsToNumber(truncate(monthlyPayment,0))}</p>
           <p>Total de intereses a pagar: ${addDotsToNumber(truncate(totalInterest,0))}</p>
-          <p>Costo total del préstamo: ${addDotsToNumber(truncate(totalCost,0))}</p>
+          <p>Costo total del préstamo: ${addDotsToNumber(truncate(totalCost,0))}
+          <span className="info-icon-result" title="Los valores varian segun los gastos operacionales de cada banco que aca NO estan aplicados.">
+        <FaInfoCircle />
+        </span>
+          </p>
+          
         </div>
       )}
     </div>
