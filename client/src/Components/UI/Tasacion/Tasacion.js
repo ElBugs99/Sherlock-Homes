0f74e2a1a5d7560087c831a2a76e5e9138840a14
OneRonnin97 <ronin.dlsr@gmail.com>
2024-06-07 10:15:52 -0400

    tasacion IA

---
+++ client/src/Components/UI/Tasacion/Tasacion.js
@@ -0,0 +1,73 @@
+import React from "react";
+import  './tasacion.css';
+import Lottie from 'react-lottie';
+import animationTasacion from '../../../assets/animation/Animation - animacionTasacion.json'
+
+
+export default function Tasacion(){
+
+
+    const animationOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationTasacion,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+
+        return(
+            <div className="Tasacion-box">
+                
+                <div className="Header-title-tasacion">
+                    <h3>Tasador Online IA</h3>
+                </div>
+
+                <div className="animacionTasacion">
+                <Lottie
+                options={animationOptions}
+                isClickToPauseDisabled={true}
+                height={100}
+                width={100}
+
+                /> 
+                </div>
+
+                 
+                
+                <div className="Info-tasacion">
+                    <ul>
+                        <li className="lista-tasacion">
+                            Completa los datos de la propiedad
+                        </li>
+                        <li className="lista-tasacion">
+                            Completa tus datos personales
+                        </li>
+                        <li className="lista-tasacion">
+                            Listo. Ahora podras ver el precio de venta sugerido por ReV, Algoritmo de IA
+                        </li>
+                    </ul>
+                    <div className="boton-tasacion-aling">
+                        <a href="https://houm.com/cl/algoritmo-precio" target="_blank" >
+                            <button className="boton-tasacion">
+                                
+                                Ir al Tasador Online
+                            </button>
+                        </a>
+                        
+                    </div>
+                    
+
+                </div>
+
+
+            </div>
+
+            
+
+
+
+        )
+
+
+}
