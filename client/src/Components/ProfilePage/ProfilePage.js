51e1af7a61ab306e8b21a273ca99f298ab45f764
ElBugs99 <jpvaldivia99@gmail.com>
2024-06-21 17:44:13 -0400

    admin basic layout

--- client/src/Components/ProfilePage/ProfilePage.js
+++ client/src/Components/ProfilePage/ProfilePage.js
@@ -202,131 +202,153 @@ export default function ProfilePage() {
     return (
         <div className='profile-page-container'>
             <NavBar searchHidden={true} />
-            {user?.role === 'admin' && <div className='p-admin'>Bienvenido Administrador</div>}
-            <div className='profile-page'>
+            <div className='p-content'>
+
                 <div className='profile-page-left'>
                     <div className='profile-c-container'>
                         <UserCard />
                     </div>
                 </div>
-                <div className='profile-page-center'>
-                    <div className='profile-center-top'>
-                        <div className='profile-info-box favorites' onClick={() => setSelectedSection('favorites')}>
-                            Favoritos <MdFavorite className='profile-icon' />
-                        </div>
-                        <div
-                            className={`profile-info-box compare ${selectedCount !== 2 ? 'disabled' : ''}`}
-                            onClick={openModal}
-                            style={{ opacity: selectedCount !== 2 ? 0.5 : 1, pointerEvents: selectedCount !== 2 ? 'none' : 'auto' }}
-                        >
-                            Comparar <MdCompareArrows className='profile-icon' />
-                        </div>
-                        {isModalOpen && (
-                            <div className="modal">
-                                <div className="modal-content" style={{
-                                    backgroundImage: `url(${Fondo})`,
-                                    backgroundSize: 'cover',
-                                    backgroundRepeat: 'no-repeat',
-                                    backgroundColor: 'rgba(255, 255, 255, 0.6)',
-                                    backgroundBlendMode: 'overlay'
-                                }}>
-                                    <button className="close-button" onClick={closeModal}>&times;</button>
-                                    <div className='text-comparar'>
-                                        <h1 className='titulo-comparar'>COMPARAR PROPIEDADES</h1>
+                <div className='profile-page'>
+
+                    <div className='profile-page-center'>
+                        {
+                            user?.role === 'admin' &&
+                            <div className='admin-content'>
+                                <div className='p-admin'>
+                                    Bienvenido Administrador
+                                </div>
+                                <div className='admin-info-box'>
+                                    <div className='admin-info-box-content'>
+                                        <div className='admin-info-box-label'>Usuarios</div>
                                     </div>
-                                    <br></br>
-                                    <div className="image-container">
-                                        {selectedFavorites.map((favorite, index) => (
-                                            <div className="image-info" key={favorite.property_id}>
-                                                <h2 className='opcion'>{`OPCIÓN ${index + 1}`}</h2>
-                                                <div className='responsive-image'>
-                                                    <CompareCarousel className='responsive-image' imageArray={favorite.media} />
-                                                </div>
-                                                <div className="info">
-                                                    <div className='campos-propiedad'>
-                                                        <h2 className='campo-nocomparar'>Nombre: </h2>
-                                                        <p className='campo-comparar'>{favorite.title}</p>
-                                                    </div>
-                                                    <div className='campos-propiedad'>
-                                                        <h2 className='campo-nocomparar'>Tipo Propiedad: </h2>
-                                                        <p className='campo-comparar'>{favorite.property_type}</p>
-                                                    </div>
-                                                    <div className='campos-propiedad'>
-                                                        <h2 className='campo-nocomparar'>Ciudad: </h2>
-                                                        <p className='campo-comparar'>{favorite.city}</p>
-                                                    </div>
-                                                    <br />
-                                                    <div className='campos-propiedad'>
-                                                        <h2 className='campo-nocomparar'>Precio: </h2>
-                                                        <p className='campo-comparar'>{`$ ${addDotsToNumber(favorite.price)}`}</p>
-                                                    </div>
-                                                    <div className='campos-propiedad'>
-                                                        <h2 className='campo-nocomparar'>m2: </h2>
-                                                        <p className='campo-comparar'>{favorite.sqft}</p>
-                                                    </div>
-                                                    <div className='campos-propiedad'>
-                                                        <h2 className='campo-nocomparar'>N° Baños: </h2>
-                                                        <p className='campo-comparar'>{favorite.bathrooms}</p>
-                                                    </div>
-                                                    <div className='campos-propiedad'>
-                                                        <h2 className='campo-nocomparar'>N° Dormitorios: </h2>
-                                                        <p className='campo-comparar'>{favorite.bedrooms}</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
+                                    <div className='admin-info-box-content'>
+                                        <div className='admin-info-box-label'>Comentarios</div>
+                                    </div>
+                                    <div className='admin-info-box-content'>
+                                        <div className='admin-info-box-label'>Favoritos</div>
                                     </div>
                                 </div>
                             </div>
+                        }
+                        <div className='profile-center-top'>
+                            <div className='profile-info-box favorites' onClick={() => setSelectedSection('favorites')}>
+                                Favoritos <MdFavorite className='profile-icon' />
+                            </div>
+                            <div
+                                className={`profile-info-box compare ${selectedCount !== 2 ? 'disabled' : ''}`}
+                                onClick={openModal}
+                                style={{ opacity: selectedCount !== 2 ? 0.5 : 1, pointerEvents: selectedCount !== 2 ? 'none' : 'auto' }}
+                            >
+                                Comparar <MdCompareArrows className='profile-icon' />
+                            </div>
+                            {isModalOpen && (
+                                <div className="modal">
+                                    <div className="modal-content" style={{
+                                        backgroundImage: `url(${Fondo})`,
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundColor: 'rgba(255, 255, 255, 0.6)',
+                                        backgroundBlendMode: 'overlay'
+                                    }}>
+                                        <button className="close-button" onClick={closeModal}>&times;</button>
+                                        <div className='text-comparar'>
+                                            <h1 className='titulo-comparar'>COMPARAR PROPIEDADES</h1>
+                                        </div>
+                                        <br></br>
+                                        <div className="image-container">
+                                            {selectedFavorites.map((favorite, index) => (
+                                                <div className="image-info" key={favorite.property_id}>
+                                                    <h2 className='opcion'>{`OPCIÓN ${index + 1}`}</h2>
+                                                    <div className='responsive-image'>
+                                                        <CompareCarousel className='responsive-image' imageArray={favorite.media} />
+                                                    </div>
+                                                    <div className="info">
+                                                        <div className='campos-propiedad'>
+                                                            <h2 className='campo-nocomparar'>Nombre: </h2>
+                                                            <p className='campo-comparar'>{favorite.title}</p>
+                                                        </div>
+                                                        <div className='campos-propiedad'>
+                                                            <h2 className='campo-nocomparar'>Tipo Propiedad: </h2>
+                                                            <p className='campo-comparar'>{favorite.property_type}</p>
+                                                        </div>
+                                                        <div className='campos-propiedad'>
+                                                            <h2 className='campo-nocomparar'>Ciudad: </h2>
+                                                            <p className='campo-comparar'>{favorite.city}</p>
+                                                        </div>
+                                                        <br />
+                                                        <div className='campos-propiedad'>
+                                                            <h2 className='campo-nocomparar'>Precio: </h2>
+                                                            <p className='campo-comparar'>{`$ ${addDotsToNumber(favorite.price)}`}</p>
+                                                        </div>
+                                                        <div className='campos-propiedad'>
+                                                            <h2 className='campo-nocomparar'>m2: </h2>
+                                                            <p className='campo-comparar'>{favorite.sqft}</p>
+                                                        </div>
+                                                        <div className='campos-propiedad'>
+                                                            <h2 className='campo-nocomparar'>N° Baños: </h2>
+                                                            <p className='campo-comparar'>{favorite.bathrooms}</p>
+                                                        </div>
+                                                        <div className='campos-propiedad'>
+                                                            <h2 className='campo-nocomparar'>N° Dormitorios: </h2>
+                                                            <p className='campo-comparar'>{favorite.bedrooms}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                            <div className='profile-info-box comments' onClick={() => setSelectedSection('comments')}>
+                                Comentarios <FaComment className='profile-icon' />
+                            </div>
+                        </div>
+                        {selectedCount !== 2 && (
+                            <p className="select-two-options-text">Seleccione solo 2 propiedades favoritas para compararlas</p>
                         )}
-                        <div className='profile-info-box comments' onClick={() => setSelectedSection('comments')}>
-                            Comentarios <FaComment className='profile-icon' />
+                        <div className='profile-center-info-box'>
+                            <div className='profile-center-info-box-content'>
+                                {selectedSection === 'favorites' && (
+                                    <div className='favorites-list'>
+                                        <TableRow row={favoritesTitleRow} isTitle={true} />
+                                        {favoritesRows.map((row, index) => (
+                                            <TableRow key={index} row={row} />
+                                        ))}
+                                    </div>
+                                )}
+                                {selectedSection === 'comments' && (
+                                    <div className='comments-list-profile'>
+                                        <TableRow row={commentsTitleRow} isTitle={true} />
+                                        {commentsRows.map((row, index) => (
+                                            <TableRow key={index} row={row} />
+                                        ))}
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     </div>
-                    {selectedCount !== 2 && (
-                        <p className="select-two-options-text">Seleccione solo 2 propiedades favoritas para compararlas</p>
-                    )}
-                    <div className='profile-center-info-box'>
-                        <div className='profile-center-info-box-content'>
-                            {selectedSection === 'favorites' && (
-                                <div className='favorites-list'>
-                                    <TableRow row={favoritesTitleRow} isTitle={true} />
-                                    {favoritesRows.map((row, index) => (
-                                        <TableRow key={index} row={row} />
-                                    ))}
-                                </div>
-                            )}
-                            {selectedSection === 'comments' && (
-                                <div className='comments-list-profile'>
-                                    <TableRow row={commentsTitleRow} isTitle={true} />
-                                    {commentsRows.map((row, index) => (
-                                        <TableRow key={index} row={row} />
-                                    ))}
-                                </div>
-                            )}
+                    <div className='profile-page-right'>
+                        <div className='profile-page-side-info-box'>
+                            <div className='house-animation-container'>
+                                <Lottie
+                                    options={houseOptions}
+                                    isClickToPauseDisabled={true}
+                                    height={200}
+                                    width={250}
+                                />
+                                <p className='house-animation-text'>
+                                    Cientos de familias han encontrado su hogar ideal gracias a Sherlock Homes. ¿Qué estás esperando?
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className='profile-page-right'>
-                    <div className='profile-page-side-info-box'>
-                        <div className='house-animation-container'>
-                            <Lottie
-                                options={houseOptions}
-                                isClickToPauseDisabled={true}
-                                height={200}
-                                width={250}
-                            />
-                            <p className='house-animation-text'>
-                                Cientos de familias han encontrado su hogar ideal gracias a Sherlock Homes. ¿Qué estás esperando?
-                            </p>
-                        </div>
-                    </div>
-                    <div className='profile-page-side-info-box info-box2'>
-                        <div className='videoPublicidad1'>
-                            <video className="publicidad1" autoPlay loop muted>
-                                <source src={Publicidad1} type="video/mp4" />
-                                Your browser does not support the video tag.
-                            </video>
+                        <div className='profile-page-side-info-box info-box2'>
+                            <div className='videoPublicidad1'>
+                                <video className="publicidad1" autoPlay loop muted>
+                                    <source src={Publicidad1} type="video/mp4" />
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
                         </div>
                     </div>
                 </div>
